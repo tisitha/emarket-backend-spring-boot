7a1bc6fdfbcd1330e7bb8fbbae87a4a4e3ed9b8f
Tisitha <tisitha007@gmail.com>
2025-10-02 14:57:56 +0530

    updated expiration time in jwt token

--- src/main/java/com/tisitha/emarket/util/JWTUtil.java
+++ src/main/java/com/tisitha/emarket/util/JWTUtil.java
@@ -34,7 +34,7 @@ public class JWTUtil {
                 .setClaims(claims)
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
